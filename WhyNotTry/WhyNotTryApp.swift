f1041bb9b20280591a5a4e79cf3a54905ef53f82
Maciej Szostak <info@reco.studio>
2025-06-21 21:25:01 +0200

    Initial Commit

---
+++ WhyNotTry/WhyNotTryApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotTryApp.swift
+//  WhyNotTry
+//
+//  Created by Maciej Szostak on 21/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotTryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
