2d8abb0cda9285fdf5770640a3e6164291005d3e
Maciej Szostak <info@reco.studio>
2025-06-22 15:34:40 +0200

    Update ContentView with code improvments

--- WhyNotTry/ContentView.swift
+++ WhyNotTry/ContentView.swift
@@ -1,36 +1,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    var activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
-    var colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
-    @State var selected = "Baseball"
+    let activities = ["Archery", "Baseball", "Basketball", "Bowling", "Boxing", "Cricket", "Curling", "Fencing", "Golf", "Hiking", "Lacrosse", "Rugby", "Squash"]
+    let colors: [Color] = [.blue, .cyan, .gray, .green, .indigo, .mint, .orange, .pink, .purple, .red]
+    @State private var selected = "Baseball"
+    @State private var selectedColor: Color = .blue
 
 
     var body: some View {
         VStack {
-            Text("Why Not try ? ")
+            Text("Why not try?")
                 .font(.largeTitle.bold())
 
             Spacer()
-            VStack {
-                Circle()
-                    .fill(colors.randomElement() ?? .blue)
-                    .padding()
-                    .overlay(
-                        Image(systemName: "figure.\(selected.lowercased())")
-                            .font(.system(size: 144))
-                            .foregroundColor(.white)
-                    )
-
-                Text("\(selected)!")
-                    .font(.title)
+            if !selected.isEmpty {
+                VStack {
+                    Circle()
+                        .fill(selectedColor)
+                        .padding()
+                        .overlay(
+                            Image(systemName: "figure.\(selected.lowercased())")
+                                .font(.system(size: 144))
+                                .foregroundColor(.white)
+                                .accessibilityLabel(Text("Activity: \(selected)"))
+                        )
+                    Text("\(selected)!")
+                        .font(.title)
+                }
+                .transition(.slide)
+                .id(selected)
             }
-
             Spacer()
 
             Button("Try again") {
                 withAnimation(.easeInOut(duration: 1)) {
                     selected = activities.randomElement() ?? "Archery"
+                    selectedColor = colors.randomElement() ?? .blue
                 }
             }
             .buttonStyle(.borderedProminent)
@@ -41,4 +46,3 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
-
